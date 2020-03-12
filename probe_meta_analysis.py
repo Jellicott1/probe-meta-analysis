fa2cec01121323bcc72b9095a17f4a06a2320383
James Ellicott <jellicott21@gmail.com>
2020-03-12 16:21:27 +0000

    Update probe_meta_analysis + datafiles
    Added polynomial and exp-weibull curve fitting as well as gradient calculation for polynomial curve. Also added some plotting functions.

--- probe_meta_analysis.py
+++ probe_meta_analysis.py
@@ -12,6 +12,8 @@ import random as rnd
 import os
 import matplotlib.pyplot as plt
 import scipy.stats as stat
+from scipy.signal import savgol_filter
+from lmfit import Model
 
 #%% Data reading functions
 
@@ -77,11 +79,11 @@ def plt_probes_per_block_hist(block_data, bins=None):
 	plt.xlabel('Number of Probes')
 	plt.title('Histogram of Probe Block Counts')
 	plt.show()
-
+	
 def plt_many_probes(data, col='p_value'):
 	plt.figure()
 	for item in data:
-		plt.plot(item.sort_values(by=col,ascending=False)[col].values)
+		plt.plot(np.linspace(1,0,len(item)),item.sort_values(by=col,ascending=False)[col].values)
 	plt.ylabel(col)
 	plt.xlabel('Sample Ranking')
 	plt.title('Superimposed plots of {} for many probes'.format(col))
@@ -105,3 +107,83 @@ stat.tstd(block_data['Unique Blocks'])
 # low_count_blocks = block_probe_counts[block_probe_counts['Unique Probes'] < block_probe_counts.describe().iloc[4,0]]
 # low_count_blocks = block_probe_counts.sort_values('Unique Probes').iloc[0:300].index
 # block_data = block_data[block_data['']]
+
+#%%
+test = data[0]['p_value'].sort_values(ascending=True).values
+x = np.linspace(0,1,len(test))
+x_nat = np.arange(1,len(test)+1)
+plt.plot(x,test)
+# plt.plot(savgol_filter(data[0]['p_value'].sort_values().values, 1581, 2))
+plt.plot(x,savgol_filter(test, 51, 4))
+
+#%%
+def f(x, x_2, x_3, x_4):
+	return x_2*x**2 + x_3*x**3 + x_4*x**4
+def expweib(x,k,lamda,alpha):
+      return alpha*(k/lamda)*((x/lamda)**(k-1))*((1-np.exp(-(x/lamda)*k))**(alpha-1))*np.exp(-(x/lamda)*k)
+def expweib_cdf(x,k,l,a):
+	return (1-np.exp(-(x/l)**k))**a
+def model_fit_f(data, data_index, weight='default', results=False):
+	raw = data[data_index]['p_value'].sort_values(ascending=True).values
+	raw = raw[~np.isnan(raw)]
+	x = np.linspace(0,1,len(raw))
+	pmodel = Model(f)
+	params = pmodel.make_params(x_2=1, x_3=0, x_4=1)
+	if weight == 'default':
+		result = pmodel.fit(raw, params, x=x, weights=np.logspace(1,0,len(raw)))
+	elif weight == None:
+		result = pmodel.fit(raw, params, x=x)
+	else:
+		result = pmodel.fit(raw, params, x=x, weights=weight)
+	y = result.eval(x=x)
+	if results == True:
+		return x, y, result
+	else:
+		return x, y, raw
+def model_fit_weib(data, data_index, weight='default'):
+	raw = data[data_index]['p_value'].sort_values(ascending=True).values
+	raw = raw[~np.isnan(raw)]
+	x = np.linspace(0,1,len(raw))
+	pmodel = Model(expweib_cdf)
+	params = pmodel.make_params(k=1,l=1,a=1)
+	params['k'].min = 0.00000000001
+	params['l'].min = 0.00000000001
+	params['a'].min = 0.00000000001
+	if weight == 'default':
+		result = pmodel.fit(raw, params, x=x, weights=np.logspace(1,0,len(raw)))
+	elif weight == None:
+		result = pmodel.fit(raw, params, x=x)
+	else:
+		result = pmodel.fit(raw, params, x=x, weights=weight)
+	y = result.eval(x=x)
+	return x, y, raw
+def f_grad(x, probe_id, coef_dict):
+	x_2, x_3, x_4 = coef_dict[probe_id].values()
+	return 2*x_2*x + 3*x_3*x**2 + 4*x_4*x**3
+#%%
+n = 6
+fig, ax = plt.subplots(1,n)
+fig2, ax2 = plt.subplots(1,2)
+ax2[0].title.set_text('Exp-Weibull')
+ax2[1].title.set_text('Poly')
+for i in range(n):
+	leg = ['Data {}'.format(i)]
+	if len(data[i])%2 == 0:
+		weight = np.concatenate((np.logspace(1,0,int(len(data[i])/2)),np.logspace(0,1,int(len(data[i])/2))))
+	else:
+		weight = np.concatenate((np.logspace(1,0,int((len(data[i])+1)/2)),np.logspace(0,1,int((len(data[i])-1)/2))))
+	x, y, test = model_fit_weib(data, i)
+	ax[i].plot(x,test)
+	ax[i].plot(x,y)
+	ax2[0].plot(x,y)
+	leg.append('\nexpweib {}\ncorr = {:.4f}\nerror = {:.2f}'.format(i, np.corrcoef(y,test)[0,1], sum(abs(y-x))))
+	x, y, test = model_fit_f(data, i)
+	ax[i].plot(x,y)
+	leg.append('\npoly {}\ncorr = {:.4f}\nerror = {:.2f}'.format(i, np.corrcoef(y,test)[0,1], sum(abs(y-x))))
+	ax[i].legend(leg)
+	ax2[1].plot(x,y)
+#%%
+# coefs = {}
+# for i in range(len(data)):
+# 	x, y, result = model_fit_f(data, i, results=True)
+# 	coefs[data[i]['probe_id'][0]] = result.best_values
